ec91a694c018af70920bbb027dfc985d717e64b9
Evelyn Valles <106193803+evelynvalles@users.noreply.github.com>
2022-07-29 17:03:22 -0500

    First Commit

---
+++ Loop Challenges/script.js
@@ -0,0 +1,27 @@
+for (var i =1; i <= 20; i++) {
+    if (i % 2 == 1) {
+        console.log(i)
+    }
+}
+
+for (var s = 100; s >- 1; s--) {
+    if (s % 3 == 0) {
+        console.log(s);
+    }
+}
+
+for (var e = 4; e >= -3.5; e-=1.5) {
+    console.log(e);
+}
+
+var sum = 0;
+for (var j = 1; j <= 100; j++) {
+    sum+= j;
+}
+console.log(sum);
+
+var product = 1;
+for (var m = 1; m < 13; m++) {
+    product*= m;
+}
+console.log(product);
